0e389167db393f2677fab9ab89caeef4bf645747
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2021-03-20 09:21:08 +0000

    Added lesson part 3

--- Aula_XVII/cmd/check.go
+++ Aula_XVII/cmd/check.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"uploader/dataparser"
 
 	"github.com/spf13/cobra"
 )
@@ -9,9 +10,14 @@ import (
 const (
 	// FILE is name of the file flag
 	FILE = "file"
+	// HASHEADER is the name of the flag for the CSV header
+	HASHEADER = "header"
 )
 
-var file string
+var (
+	file      string
+	hasHeader bool
+)
 
 // checkCmd represents the check command
 var checkCmd = &cobra.Command{
@@ -20,12 +26,15 @@ var checkCmd = &cobra.Command{
 	Long: `The check command basically validates if the csv files is a valid
 csv file to be used by the jamjam store`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fileName, err := cmd.Flags().GetString(FILE)
+		fileName, err := getFilename(cmd)
 		if err != nil {
 			return err
 		}
-		fmt.Println(fileName)
-		return nil
+		hasH, err := getHasHeader(cmd)
+		if err != nil {
+			return err
+		}
+		return checkCSVFile(fileName, hasH)
 	},
 }
 
@@ -34,9 +43,80 @@ func init() {
 	checkCmd.Flags().StringVarP(
 		&file,
 		FILE,
-		"f",
+		string(FILE[0]),
 		"",
 		"The csv file to be validated",
 	)
+	checkCmd.Flags().BoolVarP(
+		&hasHeader,
+		HASHEADER,
+		string(HASHEADER[1]),
+		false,
+		"If the CSV file has a col title header or not",
+	)
 	checkCmd.MarkFlagRequired(FILE)
+}
+
+func getFilename(c *cobra.Command) (string, error) {
+	fileName, err := c.Flags().GetString(FILE)
+	if err != nil {
+		return "", err
+	}
+	return fileName, nil
+}
+
+func getHasHeader(c *cobra.Command) (bool, error) {
+	header, err := c.Flags().GetBool(HASHEADER)
+	if err != nil {
+		return false, err
+	}
+	return header, nil
+}
+
+func checkCSVFile(f string, hasH bool) error {
+	file, err := dataparser.Read(f)
+	if err != nil {
+		return err
+	}
+	products, err := dataparser.CSV(file, hasH)
+	if err != nil {
+		return err
+	}
+	adder := 1
+	if hasH {
+		adder++
+	}
+	for index, product := range products {
+		if product.Name == "" {
+			fmt.Printf(
+				"Product name of row %d faulty\n",
+				index+adder,
+			)
+		}
+		if product.Desc == "" {
+			fmt.Printf(
+				"Product description of row %d faulty\n",
+				index+adder,
+			)
+		}
+		if product.Category == "" {
+			fmt.Printf(
+				"Product category of row %d doesn't exist\n",
+				index+adder,
+			)
+		}
+		if product.Image == "" {
+			fmt.Printf(
+				"Product image of row %d can't be empty\n",
+				index+adder,
+			)
+		}
+		if _, err := dataparser.Read(product.Image); err != nil { //TODO: don't forget to concatenate images path with image name!
+			fmt.Printf(
+				"Image of row %d couldn't be found or open\n",
+				index+adder,
+			)
+		}
+	}
+	return nil
 }
